69ceb7e68b26b52df3b25a1e9dfc06e19e9be251
aunal16 <aunal16@ku.edu.tr>
2021-10-21 15:40:47 +0300

    video is splitted into frames

--- video_to_ppg.py
+++ video_to_ppg.py
@@ -8,10 +8,26 @@ video_folder_dir = os.path.join(cwd, 'Videos')
 video_file_name  = 'IMG_3836.MOV'
 
 video_dir = os.path.join(video_folder_dir, video_file_name)
-vidcap = cv2.VideoCapture(video_dir)
 
-# check whether saved before
+# check whether saved before. if not, create the file
 frame_folder_dir = os.path.join(cwd, 'Frames')
-check = utils.check_if_saved_before(frame_folder_dir, video_file_name)
+path, check = utils.check_if_saved_before(frame_folder_dir, video_file_name)
+
+if not check:
+    # if video is not splitted into frames before
+    frames = list()
+    vidcap = cv2.VideoCapture(video_dir)
+    # get first frame
+    success,image = vidcap.read()
+
+    count = 0
+    while success:
+        frames.append(image)
+        os.chdir(path)
+        cv2.imwrite("frame%d.jpg" % count, image)     # save frame as JPEG file
+        success,image = vidcap.read()
+        count += 1
+
+
 
 
